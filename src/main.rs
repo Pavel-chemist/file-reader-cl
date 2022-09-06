8105ded352090a77161c368c6c083723b59e4658
Pavel-chemist <pavel.chemist@gmail.com>
2022-09-06 10:47:23 +0400

    add formatting to terminal output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io;
 
 fn main() {
     println!("`ls`");
@@ -11,25 +12,63 @@ fn main() {
     }
 
     let mut file_buff: Vec<u8> = Vec::new();
+    
 
-    match fs::read("test/test.txt") {
+    match fs::read("test/colors.bmp") {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(buff) => {
             file_buff = buff;
         }
     };
 
-    for i in 0..file_buff.len() {
-        print!("{} ", byte_to_hex_string(file_buff[i]));
-    }
+    print_file_buffer(file_buff);
+    
 
     println!("");
+}
 
-    for i in 0..file_buff.len() {
-        print!("{}  ", byte_to_char(file_buff[i]));
+fn print_file_buffer(buff: Vec<u8>) {
+    let mut index: usize;
+    let mut answer: String = String::from("");
+
+    for j in 0..(buff.len() / 16 + 1) {
+        print!("{}\t|  ", j + 1);
+        for i in 0..16 {
+            index = j * 16 + i;
+            if index < buff.len() {
+                print!("{} ", byte_to_hex_string(buff[index]));
+            } else {
+                print!("   ");
+            }
+            if (i + 1) % 4 == 0 {
+                print!(" ")
+            }
+            
+            if i == 7 {
+                print!(" ")
+            }        
+        }
+        print!("|  ");
+        for i in 0..16 {
+            index = j * 16 + i;
+            if index < buff.len() {
+                print!("{}", byte_to_char(buff[index]));
+            } else {
+                print!(" ");
+                answer = "n".to_string();
+            }
+        }
+        println!("  |");
+        
+        if (j + 1) % 16 == 0 {
+            println!("Enter to continue, any key to abort:");
+            answer = read_input("qwer");
+            if answer != "" {
+                println!("answer: {}", answer);
+                break;
+            }
+        }
     }
-
-    println!("");
 }
 
 fn byte_to_hex_string(byte: u8) -> String {
@@ -58,7 +97,47 @@ fn nybble_to_char(nybble: u8) -> char {
 fn byte_to_char(byte: u8) -> char {
     if byte >= 32 && byte < 128 {
         return byte as char;
+    } else if byte == 0 {
+        return '░';
+    } else if byte == 255 {
+        return '█';
     } else {
         return 26 as char;
     }
+}
+
+fn read_input<T: std::str::FromStr>(error_message: &str) -> T {
+    // generic function that reads input and checks for type (number or text)
+    // the error handling should be here
+    let mut input: String = String::new();
+    let result: T;
+    let error: &str;
+
+    if error_message.trim() == "" {
+        error = "use correct value type, e.g. number";
+    } else {
+        error = error_message;
+    }
+
+    loop {
+        io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read the line");
+
+        input = input.trim().to_string();
+
+        match input.parse::<T>() {
+            Ok(parsed_value) => {
+                result = parsed_value;
+                break;
+            },
+            Err(_) => {
+                println!("{}", error);
+                input = String::new();
+                continue;
+            },
+        };
+    }
+
+    return result;
 }
